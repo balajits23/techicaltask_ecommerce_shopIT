d856f514035fc3396362dd3876e2864a1223e2b7
Balaji <balajigowda32@gmail.com>
2022-04-30 23:37:50 +0530

    ecommerce commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*price range*/
+
+$("#sl2").slider();
+
+var RGBChange = function () {
+  $("#RGB").css(
+    "background",
+    "rgb(" + r.getValue() + "," + g.getValue() + "," + b.getValue() + ")"
+  );
+};
